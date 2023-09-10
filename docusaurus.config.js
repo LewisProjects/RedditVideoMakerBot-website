1866db21ba801e096a383cf2b5f0cb485ff80bfc
Simon <65854503+OpenSourceSimon@users.noreply.github.com>
2023-09-10 12:51:29 +0200

    Edit edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,7 +32,7 @@ const config = {
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
-						'https://github.com/LewisProjects/reddit-bot-docs-website/tree/master/',
+						'https://github.com/LewisProjects/RedditVideoMakerBot-website/blob/master/',
 				},
 				theme: {
 					customCss: require.resolve('./src/css/custom.css'),
